234f48035fee806fdf5f9f35d8c74af9ee189461
KSK10000 <90445722+KSK10000@users.noreply.github.com>
2022-01-18 16:02:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    Canvas=createCanvas(300,290);
+    Canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('downloaded.png');
+}
